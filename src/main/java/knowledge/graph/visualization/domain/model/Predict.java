9b3fe50981577196c17869c22abbaa569582140a
ly710 <ly710@outlook.com>
2020-10-22 17:07:54 +0800

    Mod fix

--- src/main/java/knowledge/graph/visualization/domain/model/Predict.java
+++ src/main/java/knowledge/graph/visualization/domain/model/Predict.java
@@ -13,4 +13,36 @@ public class Predict extends AbstractModel {
     private Long count;
 
     private Integer keep;
+
+    public String getDataset() {
+        return dataset;
+    }
+
+    public void setDataset(String dataset) {
+        this.dataset = dataset;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Long getCount() {
+        return count;
+    }
+
+    public void setCount(Long count) {
+        this.count = count;
+    }
+
+    public Integer getKeep() {
+        return keep;
+    }
+
+    public void setKeep(Integer keep) {
+        this.keep = keep;
+    }
 }
